efae636095fa7ae2718c68b76cd27d87ce4373eb
aqua2504 <aakriti2504@gmail.com>
2022-01-30 00:12:16 -0800

    final

--- Mapping_Lines/static/js/logicskilldrill.js
+++ Mapping_Lines/static/js/logicskilldrill.js
@@ -6,20 +6,22 @@ let map = L.map('mapid').setView([37.6213, -122.3790], 5);
 
 // Coordinates for each point to be used in the polyline.
 let line = [
-    [33.9416, -118.4085],
-    [37.6213, -122.3790],
     [40.7899, -111.9791],
-    [47.4502, -122.3088]
+    [30.1900, -97.6687],
+    [43.6775, -79.6308333333],
+    [40.641766, -73.780968]
   ];
 
 // Create a polyline using the line coordinates and make the line yellow.
 L.polyline(line, {
-    color: "yellow"
+    color: "blue", 
+    weight: 4,
+    opacity: 0.5
  }).addTo(map);
 
 
-// Get data from cities.js
-let cityData = cities;
+/* // Get data from cities.js
+let cityData = cities; */
 
 /* // Loop through the cities array and create one marker for each city.
 cityData.forEach(function(city) {
@@ -36,7 +38,7 @@ cityData.forEach(function(city) {
   }); */
 
 // We create the tile layer that will be the background of our map.
-let streets = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/satellite-streets-v11/tiles/{z}/{x}/{y}?access_token={accessToken}', {
+let streets = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/light/tiles/{z}/{x}/{y}?access_token={accessToken}', {
 attribution: 'Map data © <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery (c) <a href="https://www.mapbox.com/">Mapbox</a>',
     maxZoom: 18,
     accessToken: API_KEY
