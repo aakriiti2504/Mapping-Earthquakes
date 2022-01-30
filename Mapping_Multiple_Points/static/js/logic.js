db432b915f5eb785dc39018866d18a713062f354
aqua2504 <aakriti2504@gmail.com>
2022-01-29 22:54:43 -0800

    final

--- Mapping_Multiple_Points/static/js/logic.js
+++ Mapping_Multiple_Points/static/js/logic.js
@@ -2,31 +2,25 @@
 console.log("working");
 
 // Create the map object with a center and zoom level.
-let map = L.map('mapid').setView([34.0522, -118.2437], 14);
+let map = L.map('mapid').setView([34.0522, -118.2437], 4);
 
 
-//  Add a marker to the map for Los Angeles, California.
-L.circleMarker([34.0522, -118.2437], {
-  radius: 300,
-  color: "black",
-  fillColor: '#ffffa1'
-}).addTo(map);
+// Get data from cities.js
+let cityData = cities;
 
+// Loop through the cities array and create one marker for each city.
+cityData.forEach(function(city) {
+    console.log(city)
+    L.circleMarker(city.location, {
+        radius: city.population/200000,
+        lineweight: 4,
+        color: "orange",
+        fillcolor: "orange"
 
-/* L.circle([34.0522, -118.2437], {
-  radius: 300,
-  color: "black",
-  fillColor: '#ffffa1'
-}).addTo(map); */
-
-
-/* // Create the map object with a center and zoom level.
-let map = L.map("mapid", {
-    center: [
-      40.7, -94.5
-    ],
-    zoom: 4
-  }); */
+    })
+    .bindPopup("<h2>" + city.city + ", " + city.state + "</h2> <hr> <h3>Population " + city.population.toLocaleString() + "</h3>")
+    .addTo(map);
+  });
 
 // We create the tile layer that will be the background of our map.
 let streets = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/dark-v10/tiles/{z}/{x}/{y}?access_token={accessToken}', {
